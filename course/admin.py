2a58fdaea8b6fd719a3ded69f90c3aef9066f0a1
DanteXGit <alisher.nugmanoff@yandex.ru>
2020-10-20 15:15:51 +0500

    added new models

--- course/admin.py
+++ course/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import LessonModel
+from .models import LessonModel, TimeCodeModel, AttachedLinkModel
 # Register your models here.
 admin.site.register(LessonModel)
+admin.site.register(TimeCodeModel)
+admin.site.register(AttachedLinkModel)
--- course/models.py
+++ course/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 
-# Create your models here.
 class LessonModel(models.Model):
     slug = models.SlugField(unique=True, verbose_name='Слаг страницы')
-    embed_url = models.URLField(unique=True, verbose_name='Ссылка на видео')
+    embed_url = models.TextField(unique=True, verbose_name='Ссылка на видео')
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     script = models.TextField(verbose_name='Скрипт урока')
 
@@ -14,3 +13,14 @@ class LessonModel(models.Model):
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
+
+
+class AttachedLinkModel(models.Model):
+    link = models.URLField(verbose_name='Ссылка')
+    attached_to = models.ForeignKey(LessonModel, on_delete=models.CASCADE)
+
+
+class TimeCodeModel(models.Model):
+    start = models.CharField(verbose_name='Старт', max_length=5)
+    finish = models.CharField(verbose_name='Конец', max_length=5)
+    attached_to = models.ForeignKey(LessonModel, on_delete=models.CASCADE)

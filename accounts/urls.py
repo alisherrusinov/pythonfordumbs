2531eeeceec4c7dd9cd71a1362448717c3021dfb
DanteXGit <alisher.nugmanoff@yandex.ru>
2020-11-14 16:52:25 +0500

    added login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from .views import login_view
+from .views import profile_view
 from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 
 urlpatterns = [
-    path('', login_view),
     path('login/', auth_views.LoginView.as_view(), name='log-in'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='/',
+        template_name='registration/logged_out.html',),
+         name='logout'),
+    path('profile/', profile_view, name='profile')
 ]
